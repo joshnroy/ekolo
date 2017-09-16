ec5cc3846174c4e13f5832bc9af32f5ed6a99023
Josh Roy <joshnroy@gmail.com>
2017-09-16 12:10:18 -0400

    got opencv image

--- scripts/naive_sound.py
+++ scripts/naive_sound.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-
 import rospy
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+bridge = CvBridge()
 
 def callback(data):
-    print "got image"
+    image = bridge.imgmsg_to_cv2(data, "passthrough")
+    cv2.imshow('image', image)
+    cv2.waitKey(1)
+
 
 if __name__ == "__main__":
     rospy.init_node("naive_sound")
